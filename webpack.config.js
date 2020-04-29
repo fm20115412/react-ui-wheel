158792e00523e86e3d1ad92ce5c307b8d3cc5927
fengmin <fengmin01@baidu.com>
2020-04-29 21:42:04 +0800

    webpack配置终于搞成功了

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
         index: './lib/index.tsx'
     },
     output: {
-        path: p.resolve(__dirname, 'dist/lib')
+        path: p.resolve(__dirname, 'dist/lib'),
+        libraryTarget: 'umd'
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js', '.jsx']
